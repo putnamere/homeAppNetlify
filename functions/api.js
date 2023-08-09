1535b354b7da096905e413304951dbd7268e0b7a
putnamere <kirby.noah@outlook.com>
2023-08-09 12:16:57 -0700

    modified:   functions/api.js

--- functions/api.js
+++ functions/api.js
@@ -6,6 +6,7 @@ const router = express.Router()
 
 router.get('/', (req, res) => {
 //    res.sendFile(__dirname + '/page/index.html')
+    console.log(__dirname)
     res.send("is it working?")
 })
 
